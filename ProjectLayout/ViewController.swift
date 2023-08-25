583560808b44e8ef98bb28ed90f6dd45954399e4
Fatih Oguz <oguzfatih39@gmail.com>
2023-08-25 18:49:59 +0300

    duzenlemeler tamamlandi

--- ProjectLayout/ViewController.swift
+++ ProjectLayout/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+   // DEFINATIONS
+    
+    @objc let myLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let width = view.frame.size.width
+        let height = view.frame.size.height
+        
+       
+        myLabel.text = "Welcome"
+        myLabel.textAlignment = .center
+        myLabel.frame = CGRect(x: width * 0.5 - width * 0.8 / 2, y: height * 0.5 - 50/2 , width: width * 0.8 , height: 50)
+        view.addSubview(myLabel)
+        myLabel.textColor = UIColor.gray
+        
+        let myButton = UIButton()
+        myButton.setTitle("Login", for: UIControl.State.normal)
+        myButton.frame = CGRect(x: width * 0.7 - 120, y: height * 0.9, width: 70, height: 50)
+        myButton.backgroundColor = UIColor.blue
+        myButton.addTarget(self, action: #selector(ViewController.myAction), for: UIControl.Event.touchUpInside)
+        view.addSubview(myButton)
     }
+    
+    @objc func myAction() {
+        myLabel.text = "Try Again"
+    }
+    }
+    
+    
+    
 
 
-}
+
+
 
